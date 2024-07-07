5bde73fe2e2ac7314515a82978c3e90887e2ffe1
Robert Albus <hello@robertalbus.com>
2024-07-07 12:27:51 -0600

    Refactor benchmarks to remove compiler macros

--- copal/benchmark/sin.cpp
+++ copal/benchmark/sin.cpp
@@ -40,140 +40,147 @@ alignas(stdx::memory_alignment_v<stdx::native_simd<T>>) std::array<T, size>
 // --------------------------------
 // benchmarks : non-simd
 
-template<std::floating_point T, int numerator, int denominator>
-struct pi_params {
+template<std::floating_point T, int numerator, int denominator, T op(T)>
+struct bm_single_sin_params {
     using Type = T;
     constexpr static T max = T(numerator) / T(denominator) * copal::num::pi_x_1<T>;
     constexpr static T min = -max;
-};                                                                               
+    static T exec(T x) { return op(x); } 
+};
 
+template<typename params>
+static void BM_copal_single_sin_v2(benchmark::State& state) {
+  using T = params::Type;
+  aligned_array(T, testInputSize) x_in =
+    randArray<T, testInputSize>(params::min, params::max);
 
-#define BM_define_copal_single(impl_name, impl)\
-template<typename params>                                   \
-static void BM_copal_##impl_name(benchmark::State& state) { \
-  using T = params::Type;                                   \
-  aligned_array(T, testInputSize) x_in =                    \
-    randArray<T, testInputSize>(params::min, params::max);  \
-                                                            \
-  for (auto _ : state) {                                    \
-    for(size_t i = 0; i < testInputSize; ++i) {             \
-      benchmark::DoNotOptimize(impl(x_in[i]));              \
-      benchmark::ClobberMemory();                           \
-    }                                                       \
-  }                                                         \
+  for (auto _ : state) {
+    for(size_t i = 0; i < testInputSize; ++i) {
+      benchmark::DoNotOptimize(params::exec(x_in[i]));
+      benchmark::ClobberMemory();
+    }
+  }
 }
 
-BM_define_copal_single(scalar_sin_lookup, copal::Scalar<float>::sin_lookup)
-BM_define_copal_single(scalar_sin_taylor, copal::Scalar<float>::sin_taylor)
-BM_define_copal_single(stdlib_sin_lookup, copal::Stdlib<float>::sin_lookup)
-BM_define_copal_single(stdlib_sin_taylor, copal::Stdlib<float>::sin_taylor)
-BM_define_copal_single(stdlib_sin_stdlib, copal::Stdlib<float>::sin_stdlib)
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 4, copal::Scalar<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 2, copal::Scalar<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 1, copal::Scalar<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  2, 1, copal::Scalar<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  4, 1, copal::Scalar<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  8, 1, copal::Scalar<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float, 16, 1, copal::Scalar<float>::sin_lookup>>);
 
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_scalar_sin_lookup<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 4, copal::Scalar<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 2, copal::Scalar<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 1, copal::Scalar<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  2, 1, copal::Scalar<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  4, 1, copal::Scalar<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  8, 1, copal::Scalar<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float, 16, 1, copal::Scalar<float>::sin_taylor>>);
 
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_scalar_sin_taylor<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 4, copal::Stdlib<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 2, copal::Stdlib<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 1, copal::Stdlib<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  2, 1, copal::Stdlib<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  4, 1, copal::Stdlib<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  8, 1, copal::Stdlib<float>::sin_lookup>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float, 16, 1, copal::Stdlib<float>::sin_lookup>>);
 
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_lookup<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 4, copal::Stdlib<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 2, copal::Stdlib<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 1, copal::Stdlib<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  2, 1, copal::Stdlib<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  4, 1, copal::Stdlib<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  8, 1, copal::Stdlib<float>::sin_taylor>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float, 16, 1, copal::Stdlib<float>::sin_taylor>>);
 
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_taylor<pi_params<float, 16, 1>>);
-
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_stdlib_sin_stdlib<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 4, copal::Stdlib<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 2, copal::Stdlib<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  1, 1, copal::Stdlib<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  2, 1, copal::Stdlib<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  4, 1, copal::Stdlib<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float,  8, 1, copal::Stdlib<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_single_sin_v2<bm_single_sin_params<float, 16, 1, copal::Stdlib<float>::sin_stdlib>>);
 
 // --------------------------------
 // benchmarks : simd
 
-#define BM_define_copal_simd(impl_name, impl)                    \
-template<typename params>                                        \
-static void BM_copal_##impl_name(benchmark::State& state) {      \
-  using T = params::Type;                                        \
-  aligned_array(T, testInputSize) x_in =                         \
-          randArray<T, testInputSize>(params::min, params::max); \
-                                                                 \
-  aligned_array(T, stdx::native_simd<T>::size()) x_out = {};     \
-  for (auto _ : state) {                                         \
-    for (size_t i = 0; i < x_in.size(); i += stdx::native_simd<T>::size()) { \
-      stdx::native_simd<T> simd_x(&x_in[i], stdx::vector_aligned); \
-      const std::size_t size = stdx::native_simd<T>::size();       \
-        size_t idx = i / size;                                     \
-        stdx::native_simd<T> simd_x_out {};                        \
-        benchmark::DoNotOptimize(simd_x_out = impl(simd_x));       \
-        benchmark::ClobberMemory();                                \
-    }                                                              \
-  }                                                                \
+template<std::floating_point T, int numerator, int denominator, stdx::native_simd<T> op(stdx::native_simd<T>)>
+struct bm_simd_sin_params {
+    using Type = T;
+    constexpr static T max = T(numerator) / T(denominator) * copal::num::pi_x_1<T>;
+    constexpr static T min = -max;
+    static stdx::native_simd<T> exec(stdx::native_simd<T> x) { return op(x); } 
+};
+
+template<typename params>
+static void BM_copal_simd_sin_v2(benchmark::State& state) {
+  using T = params::Type;
+  aligned_array(T, testInputSize) x_in =
+    randArray<T, testInputSize>(params::min, params::max);
+
+  aligned_array(T, stdx::native_simd<T>::size()) x_out = {};
+  for (auto _ : state) {
+    for (size_t i = 0; i < x_in.size(); i += stdx::native_simd<T>::size()) {
+      stdx::native_simd<T> simd_x(&x_in[i], stdx::vector_aligned);
+      const std::size_t size = stdx::native_simd<T>::size();
+        size_t idx = i / size;
+        benchmark::DoNotOptimize(params::exec(simd_x));
+        benchmark::ClobberMemory();
+    }
+  }
 }
 
-BM_define_copal_simd(vector_impl_sin_lookup, copal::VectorImpl<float>::sin_lookup)
-BM_define_copal_simd(vector_impl_sin_taylor, copal::VectorImpl<float>::sin_taylor)
-BM_define_copal_simd(vector_stdx_sin_lookup, copal::VectorStdx<float>::sin_lookup)
-BM_define_copal_simd(vector_stdx_sin_taylor, copal::VectorStdx<float>::sin_taylor)
-BM_define_copal_simd(vector_stdx_sin_stdlib, copal::VectorStdx<float>::sin_stdlib)
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 4, copal::VectorImpl<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 2, copal::VectorImpl<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 1, copal::VectorImpl<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  2, 1, copal::VectorImpl<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  4, 1, copal::VectorImpl<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  8, 1, copal::VectorImpl<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float, 16, 1, copal::VectorImpl<float>::sin_lookup>>);
 
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_lookup<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 4, copal::VectorImpl<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 2, copal::VectorImpl<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 1, copal::VectorImpl<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  2, 1, copal::VectorImpl<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  4, 1, copal::VectorImpl<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  8, 1, copal::VectorImpl<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float, 16, 1, copal::VectorImpl<float>::sin_taylor>>);
 
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_vector_impl_sin_taylor<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 4, copal::VectorStdx<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 2, copal::VectorStdx<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 1, copal::VectorStdx<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  2, 1, copal::VectorStdx<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  4, 1, copal::VectorStdx<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  8, 1, copal::VectorStdx<float>::sin_lookup>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float, 16, 1, copal::VectorStdx<float>::sin_lookup>>);
 
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_lookup<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 4, copal::VectorStdx<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 2, copal::VectorStdx<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 1, copal::VectorStdx<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  2, 1, copal::VectorStdx<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  4, 1, copal::VectorStdx<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  8, 1, copal::VectorStdx<float>::sin_taylor>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float, 16, 1, copal::VectorStdx<float>::sin_taylor>>);
 
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_taylor<pi_params<float, 16, 1>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 4, copal::VectorStdx<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 2, copal::VectorStdx<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  1, 1, copal::VectorStdx<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  2, 1, copal::VectorStdx<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  4, 1, copal::VectorStdx<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float,  8, 1, copal::VectorStdx<float>::sin_stdlib>>);
+BENCHMARK(BM_copal_simd_sin_v2<bm_simd_sin_params<float, 16, 1, copal::VectorStdx<float>::sin_stdlib>>);
 
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float,  1, 4>>);
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float,  1, 2>>);
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float,  1, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float,  2, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float,  4, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float,  8, 1>>);
-BENCHMARK(BM_copal_vector_stdx_sin_stdlib<pi_params<float, 16, 1>>);
+
+
+
+#define asdfasdf(bm_func, type_params, type_float, impl_class, impl_method)\
+BENCHMARK(bm_func<type_params<type_float,  1, 4, impl_class<type_float>::impl_method>>);\
+BENCHMARK(bm_func<type_params<type_float,  1, 2, impl_class<type_float>::impl_method>>);\
+BENCHMARK(bm_func<type_params<type_float,  1, 1, impl_class<type_float>::impl_method>>);\
+BENCHMARK(bm_func<type_params<type_float,  2, 1, impl_class<type_float>::impl_method>>);\
+BENCHMARK(bm_func<type_params<type_float,  4, 1, impl_class<type_float>::impl_method>>);\
+BENCHMARK(bm_func<type_params<type_float,  8, 1, impl_class<type_float>::impl_method>>);\
+BENCHMARK(bm_func<type_params<type_float, 16, 1, impl_class<type_float>::impl_method>>);\
+
+asdfasdf(BM_copal_single_sin_v2, bm_single_sin_params, float, copal::Scalar, sin_lookup)

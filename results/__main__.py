ba4bc63a302836f6bd83229cbd8fcd6d7a632225
Robert Albus <hello@robertalbus.com>
2024-07-31 09:02:20 -0600

    Refactor console string parsing in Python result parsing script

--- results/__main__.py
+++ results/__main__.py
@@ -16,43 +16,14 @@ standard_header = [
     "cpu time",
 ]
 
-def parse_benchmark_result(line: str) -> BenchmarkResult:
-    parts = line.split()
-
-    instruction_type = "simd" if "simd" in parts[0] else "scalar"
-    precision = "double" if "double" in parts[1] else "float"
-
-    parts[1] = parts[1].replace(',', '')
-    parts[2] = parts[2].replace(',', '')
-    if parts[2] == "1":
-        test_range_string = f"± {parts[1]} pi"
-        test_range_value = float(parts[1])
-    else:
-        test_range_string = f"± {parts[1]}/{parts[2]} pi"
-        test_range_value = float(parts[1]) / float(parts[2])
-
-    implementation_class = parts[3].split('<')[0]
-    method = parts[3].split('>::')[1].split(">")[0]
-    time = int(parts[4])
-    time_unit = parts[5]
-    return BenchmarkResult(
-        instruction_type,
-        precision,
-        test_range_string,
-        test_range_value,
-        implementation_class,
-        method,
-        time,
-        time_unit
-    )
-
 def main(file_path: str):
     results: List[BenchmarkResult] = []
     with open(file_path, 'r') as file:
         for line in file:
             if "BM_copal_sin" in line:
-                result = parse_benchmark_result(line)
-                results.append(result)
+                results.append(
+                    BenchmarkResult.from_console_string(line)
+                )
 
     table = Table(
         table_data=[standard_header] + [result.to_list() for result in results],
--- results/lib/benchmark_result.py
+++ results/lib/benchmark_result.py
@@ -14,6 +14,36 @@ class BenchmarkResult:
         self.time = time
         self.time_unit = time_unit
 
+    @staticmethod
+    def from_console_string() -> BenchmarkResult:
+        parts = line.split()
+
+        instruction_type = "simd" if "simd" in parts[0] else "scalar"
+        precision = "double" if "double" in parts[1] else "float"
+
+        parts[1] = parts[1].replace(',', '')
+        parts[2] = parts[2].replace(',', '')
+        if parts[2] == "1":
+            test_range_string = f"± {parts[1]} pi"
+            test_range_value = float(parts[1])
+        else:
+            test_range_string = f"± {parts[1]}/{parts[2]} pi"
+            test_range_value = float(parts[1]) / float(parts[2])
+
+        implementation_class = parts[3].split('<')[0]
+        method = parts[3].split('>::')[1].split(">")[0]
+        time = int(parts[4])
+        time_unit = parts[5]
+        return BenchmarkResult(
+            instruction_type,
+            precision,
+            test_range_string,
+            test_range_value,
+            implementation_class,
+            method,
+            time,
+            time_unit
+        )
 
     def to_list(self) -> List[str]:
         return [
